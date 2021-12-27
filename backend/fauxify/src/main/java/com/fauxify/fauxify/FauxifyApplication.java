bc5c223ff141b729c7f754a0ac9001a441af13e9
saskaradit <saskaradit@gmail.com>
2021-12-27 20:57:13 +0700

    init commit

---
+++ backend/fauxify/src/main/java/com/fauxify/fauxify/FauxifyApplication.java
@@ -0,0 +1,13 @@
+package com.fauxify.fauxify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FauxifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FauxifyApplication.class, args);
+	}
+
+}
---
+++ backend/fauxify/src/test/java/com/fauxify/fauxify/FauxifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fauxify.fauxify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FauxifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
