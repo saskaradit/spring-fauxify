da244671ea00c447adebfd11a4f40df1d3ba2a80
saskaradit <saskaradit@gmail.com>
2022-01-03 15:10:24 +0700

    api register test

---
+++ frontend/src/api/apiCalls.spec.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+import * as apiCalls from './apiCalls'
+
+describe('API', () => {
+  describe('signup', () => {
+    it('calls /api/v1/users', () => {
+      const mockSignup = jest.fn()
+
+      axios.post = mockSignup
+      apiCalls.signup()
+
+      mockSignup.mock.calls[0][0]
+      expect(path).toBe('/api/v1/users')
+    })
+  })
+})
--- frontend/src/pages/UserSignupPage.js
+++ frontend/src/pages/UserSignupPage.js
@@ -18,42 +18,52 @@ const UserSignupPage = (props) => {
   }
 
   return (
-    <div>
-      <h1>Sign Up</h1>
-      <div>
+    <div className='container'>
+      <h1 className='text-center'>Sign Up</h1>
+      <div className='col-12 mb-3'>
+        <label>Display Name</label>
         <input
           type='text'
+          className='form-control'
           placeholder='Display Name'
           value={displayName}
           onChange={(e) => setDisplayName(e.target.value)}
         />
       </div>
-      <div>
+      <div className='col-12 mb-3'>
+        <label>Username</label>
         <input
+          className='form-control'
           type='text'
           placeholder='Username'
           value={username}
           onChange={(e) => setUsername(e.target.value)}
         />
       </div>
-      <div>
+      <div className='col-12 mb-3'>
+        <label>Password</label>
         <input
+          className='form-control'
           type='password'
           placeholder='Password'
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
       </div>
-      <div>
+      <div className='col-12 mb-3'>
+        <label>Confirm Password</label>
         <input
+          className='form-control'
           type='password'
           placeholder='Confirm Password'
           value={confirmPassword}
           onChange={(e) => setconfirmPassword(e.target.value)}
         />
       </div>
-      <div>
-        <button onClick={submit}>Sign Up</button>
+      <div className='text-center'>
+        <button className='btn btn-primary' onClick={submit}>
+          Sign Up
+        </button>
       </div>
     </div>
   )
